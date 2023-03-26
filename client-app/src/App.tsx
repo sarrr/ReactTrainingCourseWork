1aca3168b1437825a012362b7b490c221e701032
scott.a.roberts <scott.a.roberts4.ctr@navy.mil>
2023-03-26 12:09:08 -0400

    End section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import { Button, Header, List, ListItem } from 'semantic-ui-react';
 
 function App() {
 
@@ -10,21 +11,18 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
     .then(response => {
-      console.log(response);
       setActivities(response.data);
     })
   },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+    <div>
+        <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
           {activities.map((activity:any) => (
-            <li key={activity.id}>{activity.title}</li>
+            <ListItem key={activity.id}>{activity.title}</ListItem>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
