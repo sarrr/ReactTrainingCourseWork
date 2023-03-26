eeb32973827bd516d882984095a8cd1f6c56b6a2
scott.a.roberts <scott.a.roberts4.ctr@navy.mil>
2023-03-26 14:01:49 -0400

    WIP section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,6 +12,11 @@ namespace API.Controllers
     public class BaseApiController : ControllerBase
     {
         
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= 
+            HttpContext.RequestServices.GetService<IMediator>();
+            
     }
 
     
