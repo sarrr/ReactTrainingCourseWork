a26249a7b6cecd9e9df747e58a1181b0c144ee8a
scott.a.roberts <scott.a.roberts4.ctr@navy.mil>
2023-03-27 15:38:39 -0400

    End of section 6 add delete method, add spiner to individual delete items

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,9 +15,10 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 export default function ActivityDashboard({ activities, selectedActivity, deleteActivity,
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
+    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting }: Props) {
 
     return (
         <Grid>
@@ -25,7 +26,8 @@ export default function ActivityDashboard({ activities, selectedActivity, delete
                 <ActivityList
                     activities={activities}
                     selectActivity={selectActivity}
-                    deleteActivity={deleteActivity} />
+                    deleteActivity={deleteActivity} 
+                    submitting={submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -36,7 +38,11 @@ export default function ActivityDashboard({ activities, selectedActivity, delete
 
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit} 
+                    submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     )
