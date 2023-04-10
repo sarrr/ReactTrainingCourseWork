0feb10d72b4c53d5a766aeea12bc89555827f242
scott.a.roberts <scott.a.roberts4.ctr@navy.mil>
2023-04-10 10:50:24 -0400

    WIP

--- client-app/src/stores/ActivityStore.ts
+++ client-app/src/stores/ActivityStore.ts
@@ -1,11 +1,28 @@
-import { makeObservable, observable } from "mobx";
+import {makeAutoObservable} from "mobx";
+import {Activity} from '../app/models/activity';
+import agent from '../app/api/agent';
 
-export class ActivityStore {
-    title = "Hello from Mobx";
+export default class ActivityStore {
+
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    initialLoading = false;
 
     constructor() {
-        makeObservable(this, {
-            title: observable
-        })
+        makeAutoObservable(this)
+    }
+
+    loadActivities = async () => {
+
+        this.initialLoading = true;
+
+        try {
+            const activities = await agent.Activities.list();
+        } catch (error) {
+            
+        }
     }
 }
+
--- client-app/src/stores/store.ts
+++ client-app/src/stores/store.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { ActivityStore } from "./ActivityStore";
+import { ActivityStore } from "./activityStore";
 
 interface Store {
     activityStore: ActivityStore
